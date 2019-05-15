d2748133c9784d2d5547cbbf613e4098ac6420d8
umu+ <aydinumut82@gmail.com>
2019-05-15 08:21:25 +0300

    docs added to test

--- tests/health.test.js
+++ tests/health.test.js
@@ -5,21 +5,49 @@ const Exception = require('../src/exception');
 const Server = require('../src/server');
 
 class Service {
+    /**
+     * @description dot will use this method to build endpoints up
+     * @static
+     * @returns Object
+     * @memberof Service
+     */
     static _configure() {
         return {
             echo: [ '*', '/:path' ]
         };
     }
 
+    /**
+     * @description dot will use this method for service name
+     * @static
+     * @returns String
+     * @memberof Service
+     */
     static _name() {
         return 'myService';
     }
 
+    /**
+     * @description endpoint A
+     * @static
+     * @param {Request} [req] http request
+     * @param {Response} [res] http response
+     * @returns Promise
+     * @memberof Service
+     */
     static async echo(req) { // ? , res
         return req.params;
     }
 
-    static async exception() { // ? , res
+    /**
+     * @description endpoint B
+     * @static
+     * @param {Request} [req] http request
+     * @param {Response} [res] http response
+     * @returns Promise
+     * @memberof Service
+     */
+    static async exception() { // req , res
         throw new Exception('fail on purpose', 403);
     }
 }
