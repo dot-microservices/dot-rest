0e90829661ac63f918fd6c7c9ee75a41ec7a5e82
umu+ <aydinumut82@gmail.com>
2019-05-15 08:21:45 +0300

    docs added to exception

--- src/exception.js
+++ src/exception.js
@@ -1,6 +1,17 @@
 'use strict';
 
+/**
+ * @description customized exception
+ * @class DotException
+ * @extends {Error}
+ */
 class DotException extends Error {
+    /**
+     *Creates an instance of DotException.
+     * @param {String} message error message
+     * @param {Number} code status code
+     * @memberof DotException
+     */
     constructor(message, code) {
         super(message);
         this.name = this.constructor.name;
