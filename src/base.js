43720c846e95056a16e2bcd3362a1664553c6687
umu+ <aydinumut82@gmail.com>
2019-05-19 10:31:58 +0300

    base class added

---
+++ src/base.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Exception = require('./exception');
+const is = require('is_js');
+const Clerq = require('clerq');
+
+/**
+ * @description Base class
+ * @class Base
+ */
+class Base {
+    /**
+     *Creates an instance of Base.
+     * @param {Object} options
+     * @memberof Base
+     */
+    constructor(options) {
+        if (is.not.undefined(options) && is.not.object(options))
+            throw new Exception('invalid options');
+
+        this._options = Object.assign({}, is.object(options) && is.not.array(options) ? options : {});
+        this._clerq();
+    }
+
+    _clerq() {
+        const options = { host: this._options.redis_host || '127.0.0.1',
+            port: this._options.redis_port || 6379, redis: this._options.redis };
+        if (this._options.cache) options.cache = this._options.cache;
+        if (this._options.delimiter) options.delimiter = this._options.delimiter;
+        if (this._options.expire) options.expire = this._options.expire;
+        if (this._options.prefix) options.prefix = this._options.prefix;
+        this._registry = new Clerq(options);
+    }
+
+    /**
+     * @description returns proper service name
+     * @param {Function} service
+     * @static
+     * @private
+     * @returns String
+     * @memberof Base
+     */
+    static _name(service) {
+        if (is.not.function(service)) throw new Exception('invalid service');
+        else if (service.hasOwnProperty('_name') && is.function(service._name))
+            return service._name();
+
+        return `${ service.name.charAt(0).toLowerCase() }${ service.name.slice(1) }`;
+    }
+
+    /**
+     * @description returns hyphenated form of camel-case string
+     * @param {String} str
+     * @static
+     * @private
+     * @returns
+     * @memberof Base
+     */
+    static _unCamelCase(str){
+        str = str.replace(/([a-z\xE0-\xFF])([A-Z\xC0\xDF])/g, '$1-$2');
+        str = str.toLowerCase();
+        return str;
+    }
+}
+
+module.exports = Base;
